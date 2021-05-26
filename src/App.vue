ea8f2dd66a54f1e3df16b9e10f5610566dd5687e
THE-LUC <thitheluc.la@gmail.com>
2021-05-26 15:46:46 +0700

    FT-5 Cleaned up unused code in App component

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,11 @@
 <template>
   <div id="app">
     App Home Page
-    <form @submit.prevent="doSignin">
-      <input v-model="fakeUser.username" type="text" />
-      <input v-model="fakeUser.password" type="text" />
-      <button>Login</button>
-    </form>
-    <p>is authenticate {{ isAuth }}</p>
-    <button
-      @click="
-        fetchBook();
-        fetchCategory();
-      "
-    >
-      Get books
-    </button>
-    <p v-if="isAuth">
-      <ul>
-        <li v-for="book in allBook.rows" :key="book.id">{{book}}</li>
-      </ul>
-    </p>
-    <br />
-    <p v-if="isAuth">
-      <ul>
-        <li v-for="item in allCategory" :key="item.id">{{item}}</li>
-      </ul>
-    </p>
   </div>
-
-  
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex';
-
-export default {
-  data() {
-    return {
-      fakeUser: {
-        username: 'admin',
-        password: 'admin'
-      },
-      bookID: null
-    };
-  },
-  computed: {
-    ...mapGetters('book',['allBook']),
-    ...mapGetters('category',['allCategory']),
-    ...mapGetters('auth',['isAuth']),
-    },
-  methods: {
-    doSignin() {
-      return this.signin(this.fakeUser);
-    },
-    ...mapActions('book',['fetchBook']),
-    ...mapActions('category',['fetchCategory']),
-    ...mapActions('auth', ['signin', 'signup'])
-  }
-};
+export default {};
 </script>
 
 <style lang="scss">
@@ -67,11 +15,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-
-  input {
-    border: 1px solid #000;
-    color: red;
-  }
 }
 
 #nav {
