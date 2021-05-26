b46fe381ce3f5027c694cd98390c66e29a9b26da
dattranzaitenllc <dat.tran@zaitenllc.com>
2021-05-26 09:08:37 +0700

    FT-6 add cors allow preflight requests for all routes

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-import express from 'express';
-import logger from 'morgan';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
-import path from 'path';
-import swagger from 'swagger-jsdoc';
-import winston from 'winston';
-import validator from 'express-validator';
-import userRouter from './server/routes/userRouter';
-import bookRouter from './server/routes/bookRouter';
-import categoryRouter from './server/routes/categoryRouter';
-import searchRouter from './server/routes/searchRouter';
-import notificationRouter from './server/routes/notificationRouter';
+import express from "express";
+import logger from "morgan";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import path from "path";
+import swagger from "swagger-jsdoc";
+import winston from "winston";
+import validator from "express-validator";
+import userRouter from "./server/routes/userRouter";
+import bookRouter from "./server/routes/bookRouter";
+import categoryRouter from "./server/routes/categoryRouter";
+import searchRouter from "./server/routes/searchRouter";
+import notificationRouter from "./server/routes/notificationRouter";
+import cors from "cors";
 
 const server = express();
 
@@ -20,15 +21,15 @@ const swaggerJSDoc = swagger;
 // swagger definition
 const swaggerDefinition = {
   info: {
-    title: 'HelloBooks API',
-    version: '1.0.0',
+    title: "HelloBooks API",
+    version: "1.0.0",
     description:
-   'An application that helps manage a library and its processes '
-   + 'like stocking, tracking and renting of books.'
+      "An application that helps manage a library and its processes " +
+      "like stocking, tracking and renting of books.",
   },
-  host: 'andela-hellobooks.herokuserver.com',
+  host: "andela-hellobooks.herokuserver.com",
 
-  basePath: '/api/v1'
+  basePath: "/api/v1",
 };
 
 // options for the swagger docs
@@ -36,35 +37,38 @@ const options = {
   // import swaggerDefinitions
   swaggerDefinition,
   // path to the API docs
-  apis: ['./server/routes/*.js']
+  apis: ["./server/routes/*.js"],
 };
 
 // initialize swagger-jsdoc
 const swaggerSpec = swaggerJSDoc(options);
 
-server.use(logger('dev'));
-server.use(express.static('./client/')); // configure static files folder
-server.use(express.static('./client/public/')); // configure static files folder
-server
-  .use('/api/docs/', express.static(path.join(__dirname, 'server/api-docs/')));
+server.use(logger("dev"));
+server.use(express.static("./client/")); // configure static files folder
+server.use(express.static("./client/public/")); // configure static files folder
+server.use(
+  "/api/docs/",
+  express.static(path.join(__dirname, "server/api-docs/"))
+);
 
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: false }));
+server.use(cors({ origin: true, credentials: true }));
 server.use(validator());
 
-server.use('/api/v1', bookRouter);
+server.use("/api/v1", bookRouter);
 
-server.use('/api/v1/category', categoryRouter);
+server.use("/api/v1/category", categoryRouter);
 
-server.use('/api/v1/users', userRouter);
+server.use("/api/v1/users", userRouter);
 
-server.use('/api/v1/search', searchRouter);
+server.use("/api/v1/search", searchRouter);
 
-server.use('/api/v1/notification', notificationRouter);
+server.use("/api/v1/notification", notificationRouter);
 
 // serve swagger
-server.get('/api/docs/hellobooks.json', (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
+server.get("/api/docs/hellobooks.json", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
   res.send(swaggerSpec);
 });
 
