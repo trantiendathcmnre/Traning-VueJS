4bb60b12cc0086980151253f07c14351af3b2abe
THE-LUC <thitheluc.la@gmail.com>
2021-05-28 15:56:23 +0700

    FT-8 Fix bugs

--- src/store/auth.js
+++ src/store/auth.js
@@ -41,7 +41,11 @@ const actions = {
         commit('setAuthState', true);
       }
     } catch (error) {
-      throw new Error(error.response.data.message);
+      console.log(error.response);
+
+      throw new Error(
+        error.response.data?.message || error.response.data[0].error
+      );
     }
   }
 };
