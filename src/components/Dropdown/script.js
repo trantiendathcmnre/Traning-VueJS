819eb3f668db9830addc600fd9bc4cbb13f169ea
Uyen <uyen.tran@successsoftware.global>
2021-05-28 12:59:05 +0700

    FT-08 Fix dropdown UI

--- src/components/Dropdown/script.js
+++ src/components/Dropdown/script.js
@@ -1,35 +1,33 @@
+import { mapActions, mapGetters } from "vuex";
 export default {
   name: "FilterCategory",
   data: () => ({
-    categories: [
-      {
-        id: 1,
-        name: "Computers & IT",
-        description: "",
-        createdAt: "2021-05-20T02:10:59.966Z",
-        updatedAt: "2021-05-20T02:10:59.966Z",
-      },
-      {
-        id: 2,
-        name: "Business applications",
-        description: "",
-        createdAt: "2021-05-20T02:10:59.966Z",
-        updatedAt: "2021-05-20T02:10:59.966Z",
-      },
-      {
-        id: 3,
-        name: "Programming",
-        description: "",
-        createdAt: "2021-05-20T02:10:59.966Z",
-        updatedAt: "2021-05-20T02:10:59.966Z",
-      },
-      {
-        id: 4,
-        name: "Software technology",
-        description: "",
-        createdAt: "2021-05-20T02:10:59.966Z",
-        updatedAt: "2021-05-20T02:10:59.966Z",
-      },
-    ],
+    selectedBook: []
   }),
+  computed: {
+    ...mapGetters("category", ["allCategory"]),
+    selectAllBook() {
+      return this.selectedBook.length === this.allCategory.length
+    },
+    selectSomeBook() {
+      return this.selectedBook.length > 0 && !this.selectAllBook
+    },
+  }
+  ,
+  methods: {
+    ...mapActions("category", ["fetchCategoryAction"]),
+    toggle() {
+      console.log(this.selectAllBook)
+      this.$nextTick(() => {
+        if (this.selectAllBook) {
+          this.selectedBook = []
+        } else {
+          this.selectedBook = this.allCategory.slice()
+        }
+      })
+    },
+  },
+  mounted() {
+    this.fetchCategoryAction()
+  }
 };
